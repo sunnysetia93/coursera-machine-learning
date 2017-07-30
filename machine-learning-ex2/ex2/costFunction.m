3e53241925804fd21dae33933697ca499cd4736c
sunnysetia <sunnysetia93@gmail.com>
2017-07-30 19:31:06 +0530

    till logistic regression

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -20,11 +20,13 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+h = sigmoid(X*theta);
 
+J = sum(-y.*log(h) - ((1-y).*log(1-h)))/m;
 
-
-
-
+for i = 1:length(theta)
+    grad(i) = sum((h-y).*X(:,i))/m;
+end
 
 
 % =============================================================
--- machine-learning-ex2/ex2/plotData.m
+++ machine-learning-ex2/ex2/plotData.m
@@ -14,10 +14,11 @@ figure; hold on;
 
 
 
+pos = find(y==1);   % finds the location in Matrix where y==1
+neg = find(y==0);
 
-
-
-
+plot(X(pos,1),X(pos,2),'r+','LineWidth',2,'MarkerSize',7);  % X(pos,1) means X[pos][1]
+plot(X(neg,1),X(neg,2),'ko','MarkerFaceColor','y','MarkerSize',7);
 
 
 % =========================================================================
