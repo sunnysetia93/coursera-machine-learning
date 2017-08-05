bb2665908d1558dcb6db8b4ea8c95269a6441fa7
sunnysetia <sunnysetia93@gmail.com>
2017-08-06 02:12:24 +0530

    final week4 assignment

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,14 +21,13 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
-
-
-
-
-
-
-
+a1 = [ones(m, 1) X];
+z2 = a1*Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(m, 1) a2];
+z3 = a2*Theta2';
+h = sigmoid(z3);
+[maxP, p] = max(h, [], 2);
 % =========================================================================
 
 
