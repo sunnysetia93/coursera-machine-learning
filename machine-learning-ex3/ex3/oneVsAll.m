3fdd87a7c3ad37128a4db91b8f581c3bb55dcb27
sunnysetia <sunnysetia93@gmail.com>
2017-08-05 18:34:51 +0530

    oneVsAll

--- machine-learning-ex3/ex3/oneVsAll.m
+++ machine-learning-ex3/ex3/oneVsAll.m
@@ -51,12 +51,13 @@ X = [ones(m, 1) X];
 
 
 
-
-
-
-
-
-
+for k = 1: num_labels
+    initial_theta = zeros(n + 1, 1);
+    options = optimset('GradObj', 'on', 'MaxIter', 50);
+    [theta, J, iterC] = ...
+      fmincg (@(t)(lrCostFunction(t, X, (y == k), lambda)), initial_theta, options); 
+    all_theta(k:k,:) = theta;
+end
 
 
 
